f2ff2214dc95711ec8d86cd77d116cf152563298
Saif_MIST_23 <saif16102004@gmail.com>
2024-12-17 03:03:24 +0600

    new file added just before lab day :)

---
+++ all_pattern.c
@@ -0,0 +1,490 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    // int n;
+    // printf("enter a number to see pattern: ");
+    // scanf("%d",&n);
+    // int row=1;
+    // while (row<=n)
+    // {
+    //     int col=1;
+    //     while (col<=row)
+    //     {
+    //         printf("%d ",col);
+    //         col++;
+    //     }
+    //     row++;
+    //     printf("\n");
+
+    // }
+
+    //------------------------------------------------------------------------------------------------
+
+    // int n;
+    // printf("enter a number to see pattern: ");
+    // scanf("%d",&n);
+    // int row=1;
+    // while (row<=n)
+    // {
+    //     int col=1;
+    //     while (col<=row)
+    //     {
+    //         if(col%2==0) printf("%d ",0);     // 0 -> col%2(even->0)
+    //         else printf("%d ",1);             // 1 -> col%2(odd->1)
+    //         col++;
+    //     }
+    //     row++;
+    //     printf("\n");
+
+    // }
+    //-----------------------------------------------------------------------------------------------------
+
+    // int n;
+    // printf("enter a number to see pattern: ");
+    // scanf("%d",&n);
+    // int row=1;
+    // while (row<=n)
+    // {
+    //     int col=1;
+    //     while (col<=row)
+    //     {
+    //         if(col%2==0) printf("B ");
+    //         else printf("A ");
+    //         col++;
+    //     }
+    //     row++;
+    //     printf("\n");
+
+    // }
+    //---------------------------------------------------------------------------------------------------
+
+    // int n;
+    // printf("enter a number to see pattern: ");
+    // scanf("%d",&n);
+    // int row=1;
+    // while (row<=n)
+    // {
+    //     int col=1;
+    //     while (col<=row)
+    //     {
+    //         printf("* ");
+    //         col++;
+    //     }
+    //     row++;
+    //     printf("\n");
+
+    // }
+    //----------------------------------------------------------------------------------------------------
+
+    // int n;
+    // printf("enter a number to see pattern: ");
+    // scanf("%d",&n);
+    // int row=1;
+    // while (row<=n)
+    // {
+    //     int col=1;
+    //     while (col<=row)
+    //     {
+    //         if(row%2==0) printf("B ");
+    //         else printf("A ");
+    //         col++;
+    //     }
+    //     row++;
+    //     printf("\n");
+
+    // }
+
+    //------------------------------------------------------------------------------------------
+
+    // int n;
+    // printf("enter a number to see pattern: ");
+    // scanf("%d",&n);
+    // int row=1;
+    // while (row<=n)
+    // {
+    //     int col=1;
+    //     while (col<=row)
+    //     {
+    //         printf("%d ",row);
+    //         col++;
+    //     }
+    //     row++;
+    //     printf("\n");
+
+    // }
+    //------------------------------------------------------------------------------------------
+    //------------------------------------------------------------------------------------------
+
+    // int n;
+    // printf("enter a numb to see pattern: ");
+    // scanf("%d",&n);
+    // for(int row=n;row>0;row--)
+    // {
+    //     for(int col=1;col<=row;col++)
+    //     {
+    //         printf("%d ",col);
+    //     }
+    //     printf("\n");
+    // }
+    //------------------------------------------------------------------------------------------
+
+    // int n;
+    // printf("enter a numb to see pattern: ");
+    // scanf("%d",&n);
+    // for(int row=n;row>0;row--)
+    // {
+    //     for(int col=1;col<=row;col++)
+    //     {
+    //         col%2==0? printf("%d ",col%2):printf("%d ",col%2);
+    //     }
+    //     printf("\n");
+    // }
+    //-----------------------------------------------------------------------------------------
+
+    // int n;
+    // printf("enter a numb to see pattern: ");
+    // scanf("%d", &n);
+    // for (int row = n; row > 0; row--)
+    // {
+    //     for (int col = 1; col <= row; col++)
+    //     {
+    //         printf("%C ", 64 + col);
+    //     }
+    //     printf("\n");
+    // }
+    //------------------------------------------------------------------------------------------
+
+    // int n;
+    // printf("enter a numb to see pattern: ");
+    // scanf("%d", &n);
+    // for (int row = n; row > 0; row--)
+    // {
+    //     for (int col = 1; col <= row; col++)
+    //     {
+    //         printf("* ");
+    //     }
+    //     printf("\n");
+    // }
+    //-------------------------------------------------------------------------------------------
+
+    // int n;
+    // printf("enter a numb to see pattern: ");
+    // scanf("%d",&n);
+    // for(int row=n;row>0;row--)
+    // {
+    //     for(int col=1;col<=row;col++)
+    //     {
+    //         row%2==0? printf("%d ",row):printf("%d ",row);
+    //     }
+    //     printf("\n");
+    // }
+    //-----------------------------------------------------------------------------------------------
+
+    // int n;
+    // printf("enter a numb to see pattern: ");
+    // scanf("%d",&n);
+    // for(int row=n;row>0;row--)
+    // {
+    //     for(int col=1;col<=row;col++)
+    //     {
+    //         row%2==0? printf("%d ",row%2):printf("%d ",row%2);
+    //     }
+    //     printf("\n");
+    // }
+    //----------------------------------------------------------------------------------------------
+
+    // int n;
+    // printf("enter a numb to see pattern: ");
+    // scanf("%d",&n);
+    // for(int row=n;row>0;row--)
+    // {
+    //     for(int col=1;col<=row;col++)
+    //     {
+    //         row%2==0? printf("%c ",64+row):printf("%c ",64+row);
+    //     }
+    //     printf("\n");
+    // }
+    //----------------------------------------------------------------------------------------------
+    //-----------------------------------------------------------------------------------------------
+
+    // int n,r,c;
+    // printf("enter a numb to see pattern: ");
+    // scanf("%d",&n);
+    // for(r=1;r<=n;r++)
+    // {
+    //     for(c=1;c<=r;c++)
+    //     {
+    //         printf("%d ",c);
+    //     }
+    //     printf("\n");
+    // }
+    // for(r=n;r>0;r--)
+    // {
+    //     for(c=1;c<=r;c++)
+    //     {
+    //         printf("%d ",c);
+    //     }
+    //     printf("\n");
+
+    // }
+    //----------------------------------------------------------------------------------------------
+
+    // int n,r,c;
+    // printf("enter a numb to see pattern: ");
+    // scanf("%d",&n);
+    // for(r=1;r<=n;r++)
+    // {
+    //     for(c=1;c<=r;c++)
+    //     {
+    //         printf("%d ",c);
+    //     }
+    //     printf("\n");
+    // }
+    // for(int r=n-1;r>0;r--)
+    // {
+    //     for(c=1;c<=r;c++)
+    //     {
+    //         printf("%d ",c);
+    //     }
+    //     printf("\n");
+
+    // }
+    //-------------------------------------------------------------------------------
+
+    // int n,r,c;
+    // printf("enter a numb to see pattern: ");
+    // scanf("%d",&n);
+    // for(r=1;r<=n;r++)
+    // {
+    //     for(c=1;c<=r;c++)
+    //     {
+    //         printf("%d ",r);
+    //     }
+    //     printf("\n");
+    // }
+    // for(int r=n-1;r>0;r--)
+    // {
+    //     for(c=1;c<=r;c++)
+    //     {
+    //         printf("%d ",r);
+    //     }
+    //     printf("\n");
+
+    // }
+    //-------------------------------------------------------------------------------------------
+
+    // int n,r,c;
+    // printf("enter a numb to see pattern: ");
+    // scanf("%d",&n);
+    // for(r=1;r<=n;r++)
+    // {
+    //     for(c=1;c<=r;c++)
+    //     {
+    //         c%2==0? printf("%c ",c+64):printf("%c ",c+64);
+    //     }
+    //     printf("\n");
+    // }
+    // for(int r=n-1;r>0;r--)
+    // {
+    //     for(c=1;c<=r;c++)
+    //     {
+    //         c%2==0? printf("%c ",c+64):printf("%c ",c+64);
+    //     }
+    //     printf("\n");
+
+    // }
+    //-------------------------------------------------------------------------------------------------
+
+    // int n,r,c;
+    // printf("enter a numb to see pattern: ");
+    // scanf("%d",&n);
+    // for(r=1;r<=n;r++)
+    // {
+    //     for(c=1;c<=r;c++)
+    //     {
+    //         c%2==0? printf("%c ",r+64):printf("%c ",r+64);
+    //     }
+    //     printf("\n");
+    // }
+    // for(int r=n-1;r>0;r--)
+    // {
+    //     for(c=1;c<=r;c++)
+    //     {
+    //         c%2==0? printf("%c ",r+64):printf("%c ",r+64);
+    //     }
+    //     printf("\n");
+
+    // }
+    //-----------------------------------------------------------------------------------------------
+    //-----------------------------------------------------------------------------------------------
+
+    // int n,r,c;
+    // printf("enter a numb to see pattern: ");
+    // scanf("%d",&n);
+
+    // for(r=1;r<=n;r++)
+    // {
+    //     for ( c = 1; c <=n; c++)
+    //     {
+    //         c+r>n? printf("%d ",c+r-n):printf("  ");
+    //     }
+    //     printf("\n");
+    // }
+    //------------------------------------------------------------------------------------------
+
+    // same upper code by anis--------------------------------------------------------------
+
+    // int n,r,c;
+    // printf("enter a numb to see pattern: ");
+    // scanf("%d",&n);
+    // for(r=1;r<=n;r++)
+    // {
+    //     for(c=1;c<=n-r;c++)
+    //     {
+    //         printf("  ");
+    //     }
+    //     for(c=1;c<=r;c++)
+    //     {
+    //         printf("%d ",c);
+    //     }
+    //     printf("\n");
+    // }
+
+
+    //-------------------------------------------------------------------------------------------
+    // int n,r,c;
+    // printf("enter a numb to see pattern: ");
+    // scanf("%d",&n);
+
+    // for(r=1;r<=n;r++)
+    // {
+    //     for ( c = 1; c <=n; c++)
+    //     {
+    //         c+r>n? (c+r)%2==0? printf("%d ",1): printf("%d ",0): printf("  ");
+    //     }
+    //     printf("\n");
+    // }
+    //-----------------------------------------------------------------------------------------
+
+    // int n,r,c;
+    // printf("enter a numb to see pattern: ");
+    // scanf("%d",&n);
+
+    // for(r=1;r<=n;r++)
+    // {
+    //     for ( c = 1; c <=n; c++)
+    //     {
+    //         c+r>n? (c+r)%2==0? printf("%c ",64+c+r-n): printf("%c ",64+c+r-n): printf("  ");
+    //     }
+    //     printf("\n");
+    // }
+    //-------------------------------------------------------------------------------------------
+
+    // int n,r,c;
+    // printf("enter a numb to see pattern: ");
+    // scanf("%d",&n);
+
+    // for(r=1;r<=n;r++)
+    // {
+    //     for ( c = 1; c <=n; c++)
+    //     {
+    //         c+r>n? printf("%d ",r):printf("  ");
+    //     }
+    //     printf("\n");
+    // }
+    //----------------------------------------------------------------------------------------------
+
+    // int n,r,c;
+    // printf("enter a numb to see pattern: ");
+    // scanf("%d",&n);
+
+    // for(r=1;r<=n;r++)
+    // {
+    //     for ( c = 1; c <=n; c++)
+    //     {
+    //         c+r>n? r%2==1? printf("%d ",1): printf("%d ",0): printf("  ");
+    //     }
+    //     printf("\n");
+    // }
+    //-----------------------------------------------------------------------------------------------
+
+    // int n,r,c;
+    // printf("enter a numb to see pattern: ");
+    // scanf("%d",&n);
+
+    // for(r=1;r<=n;r++)
+    // {
+    //     for ( c = 1; c <=n; c++)
+    //     {
+    //         c+r>n? r%2==1? printf("%c ",64+r): printf("%c ",64+r): printf("  ");
+    //     }
+    //     printf("\n");
+    // }
+    //-------------------------------------------------------------------------------------------------
+
+    // int n, r, c;
+    // printf("enter a numb to see pattern: ");
+    // scanf("%d", &n);
+
+    // for (r = 1; r <= n; r++)
+    // {
+    //     for (c = 1; c <= n; c++)
+    //     {
+    //         c + r > n ? printf("* ") : printf("  ");
+    //     }
+    //     printf("\n");
+    // }
+    //--------------------------------------------------------------------------------------------------
+
+    // taken from anis
+
+    // int n, r, c;
+    // printf("enter a numb to see pattern: ");
+    // scanf("%d", &n);
+    // for ( r = n; r > 0; r--)
+    // {
+    //     for(c=1;c<=n-r;c++)
+    //     {
+    //         printf("  ");
+    //     }
+    //     for(c=1;c<=r;c++)
+    //     {
+    //         printf("%d ",c);
+    //     }
+    //     printf("\n");
+    // }
+    //-------------------------------------------------------------------------------------------------
+
+
+    // int n, r, c;
+    // printf("enter a numb to see pattern: ");
+    // scanf("%d", &n);
+    // for(r=1;r<=n;r++)
+    // {
+    //     for(c=1;c<=n-r;c++)
+    //     {
+    //         printf("  ");
+    //     }
+    //     for(c=1;c<=r;c++)
+    //     {
+    //         printf("%d ",c);
+    //     }
+    //     printf("\n");
+    // }
+    // for ( r = n-1; r > 0; r--)
+    // {
+    //     for(c=1;c<=n-r;c++)
+    //     {
+    //         printf("  ");
+    //     }
+    //     for(c=1;c<=r;c++)
+    //     {
+    //         printf("%d ",c);
+    //     }
+    //     printf("\n");
+    // }
+    //-----------------------------------------------------------------------------------------------
+
+    
+}
